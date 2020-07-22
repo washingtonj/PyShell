eed3507a512fd3f198b51c2b621a4aea51e57d1a
Washington Junior <washingtonj@Washingtons-MacBook-Pro.local>
2020-07-22 20:50:50 -0300

    Criação de um modulo para Whiptail

---
+++ whiptail/bin/runner.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+
+def stdin(command):
+    return os.system(command)
+
+def stdout(command):
+    popen = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE)
+    return popen.stderr.read().decode("utf-8")
+
+def stdcheck(command):
+    return subprocess.check_output(command, shell=True).decode("utf-8")
---
+++ whiptail/start.py
@@ -0,0 +1,73 @@
+from bin.runner import stdin, stdout, stdcheck
+
+
+class whiptail:
+    def __init__(self, widht, height):
+        self.widht = widht
+        self.height = height
+
+    def msgbox(self, title, text):
+        return stdin(
+            f"whiptail --title {title} --msgbox {text} {self.widht} {self.height}")
+
+    def yesno(self, title, text):
+        return stdin(
+            f"whiptail --title {title} --yesno {text} {self.widht} {self.height}")
+
+    def infobox(self, title, text):
+        return stdin(
+            f"whiptail --title {title} --infobox {text} {self.widht} {self.height}")
+
+    def inputbox(self, title, text):
+        return stdout(
+            f"whiptail --title {title} --inputbox {text} {self.widht} {self.height}")
+
+    def passwordbox(self, title, text):
+        return stdout(
+            f"whiptail --title {title} --passwordbox {text} {self.widht} {self.height}")
+
+
+    def checklist(self, title, text, options):
+        items = []
+        for index in options:
+            items.append(
+                f'"{list(options.keys()).index(index)}" "{index} - {options.get(index)}" OFF ')
+        test = "".join(items)
+        return stdout(
+            f'whiptail --title "{title}" --checklist --fb "{text}" {self.widht} {self.height} 12 {test} 3>&1 1>&2 2>&3')
+
+    def radiolist(self, title, text, options):
+        items = []
+        for index in options:
+            items.append(
+                f'"{list(options.keys()).index(index)}" "{index} - {options.get(index)}" OFF ')
+        test = "".join(items)
+        return stdcheck(
+            f'whiptail --title "{title}" --radiolist --fb "{text}" {self.widht} {self.height} 12 {test} 3>&1 1>&2 2>&3')
+
+
+
+    # # to implement
+    # def gauge(self, title, text):
+    #     return stdin(f'whiptail --title {title} --gauge {text} {self.widht} {self.height}')
+
+    # # to implement
+    # def textbox(self, title, file):
+    #     print("to implement")
+
+    # # to implement
+    # def menu(self, title, list):
+    #     print("to implement")
+
+# test = whiptail(20, 120)
+
+# test.msgbox("msg", "Testing")
+# test.yesno("msg", "testing")
+# test.infobox("msg", "testing")
+# test.inputbox("msg", "testing")
+# test.passwordbox("msg", "testing")
+
+# lista = {"Docker": "Docker as Docker", "Docker-compose": "Is a docker compose"}
+
+# print(test.radiolist("Testando", "Este item foi criado para teste", lista))
+# print(test.checklist("Testando", "Este item foi criado para teste", lista))
